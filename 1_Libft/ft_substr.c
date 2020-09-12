364f26dd42f5ee9eeea7300e1e5dcc983d7a912a
zeroet <y36609672@gmail.com>
2020-09-12 19:27:31 +0200

    ft_substr

---
+++ 1_Libft/ft_substr.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+
+char		*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*tmp;
+	int		size;
+	char	*res;
+
+	size = len - start;
+	if (!(tmp = (char*)malloc(sizeof(char) * size + 1)))
+		return (NULL);
+	res = tmp;
+	while (size--)
+	{
+		*tmp = s[start];
+		tmp++;
+		start++;
+	}
+	tmp = 0;
+	return (res);
+}
