addb91da9f05e5d2eadfa83892895fd3f949ff39
zeroet <y36609672@gmail.com>
2020-09-13 23:26:17 +0200

    libft.h

---
+++ 1_Libft/libft.h
@@ -0,0 +1,14 @@
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlcat(char *dest, const char *src, size_t size);
+size_t	strlcpy(char *dest, const char *src, size_t size);
+size_t	strlen(const char *s)
+char	*ft_strnstr(const char *s1, const char *s2, size_t len);
+char	*ft_strrchr(const char *s, int c);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+
+
+#endif
