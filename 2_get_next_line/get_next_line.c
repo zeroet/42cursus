aec589f1a966af1c403065fb817e8258833f1d61
zeroet <y36609672@gmail.com>
2020-12-29 17:16:58 +0100

    reset

--- 2_get_next_line/get_next_line.c
+++ 2_get_next_line/get_next_line.c
@@ -6,10 +6,16 @@
 /*   By: seyun <seyun@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/28 19:33:56 by seyun             #+#    #+#             */
-/*   Updated: 2020/12/28 19:38:37 by seyun            ###   ########.fr       */
+/*   Updated: 2020/12/29 16:28:17 by seyun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
 int	get_next_line(int fd, char **line)
 {
+	static char buf[BUFF_SIZE + 1];
 	
+	if ((fd < 0) || (line == 0) || (BUFF_SIZE < 0))
+		return (-1);
+
