4087d3afe95e2e83f8232d0eefe3ac4708ff873a
zeroet <y36609672@gmail.com>
2021-01-06 22:59:28 +0100

    strmapi

--- 1_Libft/ft_strmapi.c
+++ 1_Libft/ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: seyun <seyun@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/10 15:17:42 by seyun             #+#    #+#             */
-/*   Updated: 2021/01/06 15:09:34 by seyun            ###   ########.fr       */
+/*   Updated: 2021/01/06 22:57:32 by seyun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,20 @@
 char				*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
 	char			*res;
+	unsigned int	len;
 	unsigned int	i;
 
-	if (!(res = malloc(ft_strlen(s) + 1)))
-		return (0);
+	if (s == 0 || f == 0)
+		return (NULL);
+	len = ft_strlen(s);
+	if (!(res = (char *)malloc(sizeof(char) * (len + 1))))
+		return (NULL);
 	i = 0;
 	while (s[i])
 	{
 		res[i] = f(i, s[i]);
 		i++;
 	}
-	res[i] = 0;
+	res[i] = '\0';
 	return (res);
 }
