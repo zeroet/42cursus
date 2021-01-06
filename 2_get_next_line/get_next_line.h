5ee97aba3ebb11fa378aa96a8ecc904126e8f09e
zeroet <y36609672@gmail.com>
2021-01-06 23:30:44 +0100

    ok

--- 2_get_next_line/get_next_line.h
+++ 2_get_next_line/get_next_line.h
@@ -6,15 +6,20 @@
 /*   By: seyun <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/06 11:40:33 by seyun             #+#    #+#             */
-/*   Updated: 2021/01/06 21:48:49 by seyun            ###   ########.fr       */
+/*   Updated: 2021/01/06 23:20:17 by seyun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef	GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include <fntl.h>
+#ifndef OPEN_MAX
+# define OPEN_MAX 32
+# endif
+
+#include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 size_t ft_strlen(char *ptr);
 
