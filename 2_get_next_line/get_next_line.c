26e2e2cd2f54e422b782bf5d722daab0bf864b19
seyun <seyun@student.42.fr>
2021-01-06 13:44:04 +0100

    GNL

--- 2_get_next_line/get_next_line.c
+++ 2_get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: seyun <seyun@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/28 19:33:56 by seyun             #+#    #+#             */
-/*   Updated: 2020/12/29 16:28:17 by seyun            ###   ########.fr       */
+/*   Updated: 2021/01/06 13:43:28 by seyun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,15 @@
 int	get_next_line(int fd, char **line)
 {
 	static char buf[BUFF_SIZE + 1];
-	
+	int		size;
+
 	if ((fd < 0) || (line == 0) || (BUFF_SIZE < 0))
 		return (-1);
+	*line = 0;
+	while (0 < (size = read(fd, buf, BUFF_SIZE)))
+	{
+		/*find \n and split */
+		if (0 < (find(buf)))
+			*line = split_line(buf);/*line * ? */
 
+		
