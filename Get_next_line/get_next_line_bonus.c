e1d4b93d419c3c1cbcd03a899d08df940840812d
seyun <seyun@student.42.fr>
2021-01-16 12:11:01 +0100

    1

--- Get_next_line/get_next_line_bonus.c
+++ Get_next_line/get_next_line_bonus.c
@@ -1,20 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: seyun <seyun@student.42.fr>                +#+  +:+       +#+        */
+/*   By: seyun <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/12/28 19:33:56 by seyun             #+#    #+#             */
-/*   Updated: 2021/01/16 11:59:02 by seyun            ###   ########.fr       */
+/*   Created: 2021/01/16 11:01:38 by seyun             #+#    #+#             */
+/*   Updated: 2021/01/16 12:07:42 by seyun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 int				find(char *backup)
 {
-	int			i;
+	int i;
 
 	i = 0;
 	while (backup[i])
@@ -64,10 +64,10 @@ int				all_return(char **backup, char **line, int size)
 	return (0);
 }
 
-int				get_next_line(int fd, char **line)
+int				get_next_line_bonus(int fd, char **line)
 {
+	static char *backup[OPEN_MAX];
 	char		buf[BUFFER_SIZE + 1];
-	static char	*backup[OPEN_MAX];
 	int			end_index;
 	int			size;
 
--- Get_next_line/get_next_line_bonus.h
+++ Get_next_line/get_next_line_bonus.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: seyun <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/01/06 11:40:33 by seyun             #+#    #+#             */
-/*   Updated: 2021/01/16 10:58:02 by seyun            ###   ########.fr       */
+/*   Created: 2021/01/16 12:01:48 by seyun             #+#    #+#             */
+/*   Updated: 2021/01/16 12:02:28 by seyun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # ifndef OPEN_MAX
 #  define OPEN_MAX 32
@@ -22,7 +22,7 @@
 # include <stdlib.h>
 
 int			get_next_line(int fd, char **line);
-int			ft_strlen(char *ptr);
+int			ft_strlen(char *s1);
 char		*ft_strjoin(char *s1, char *s2);
 int			ft_strlcpy(char *dest, const char *src, int size);
 int			ft_strlcat(char *dest, const char *src, int size);
