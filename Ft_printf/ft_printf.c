9ea95c07d9b2a961b79034106216cfa79f29cce3
seyun <seyun@student.42.fr>
2021-01-22 14:39:47 +0100

    j1

--- Ft_printf/ft_printf.c
+++ Ft_printf/ft_printf.c
@@ -6,11 +6,23 @@
 /*   By: seyun <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/21 11:06:28 by seyun             #+#    #+#             */
-/*   Updated: 2021/01/21 11:23:30 by seyun            ###   ########.fr       */
+/*   Updated: 2021/01/22 14:39:34 by seyun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf.f"
+
+int			run_printf(va_list ap, char *format)
+{
+	
+
 int			ft_printf(const char *format, ...)
 {
-}
+	va_list	ap;
+	int		res;
 
+	va_start(ap, format);
+	res = run_printf(ap, (char*)format);
+	va_end(ap);
+	return (res);
+}
