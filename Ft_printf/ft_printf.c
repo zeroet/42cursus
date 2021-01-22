820907eb69e267de45ce70febceee74cef6ebb54
seyun <seyun@student.42.fr>
2021-01-22 15:25:28 +0100

    j1

--- Ft_printf/ft_printf.c
+++ Ft_printf/ft_printf.c
@@ -6,15 +6,35 @@
 /*   By: seyun <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/21 11:06:28 by seyun             #+#    #+#             */
-/*   Updated: 2021/01/22 14:39:34 by seyun            ###   ########.fr       */
+/*   Updated: 2021/01/22 15:18:40 by seyun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.f"
 
+void		check_flag(char *format, t_info *info)
+{
+	if (*format == 0 || 
+}
+
 int			run_printf(va_list ap, char *format)
 {
-	
+	int i;
+	int res;
+	t_info *info;
+
+	i = 0;
+	res = 0;
+	info = 0;
+	while (format[i] != '\0')
+	{
+		init_info(info);
+		if (format[i] != '%')
+			res += ft_putchar(format[i++]);
+		while (format[++i] != '%' && format[i] != '\0')
+			check_flag(format[i], info);
+
+}
 
 int			ft_printf(const char *format, ...)
 {
