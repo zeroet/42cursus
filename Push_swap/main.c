5d249ab71c384789d18ef4ae5125f74b03045c92
seyun <seyun@student.42.fr>
2021-06-26 15:02:20 +0200

    main.c

---
+++ Push_swap/main.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seyun <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/26 13:50:54 by seyun             #+#    #+#             */
+/*   Updated: 2021/06/26 15:02:10 by seyun            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+static void	init_stack(t_list **list, t_list *node)
+{
+	if (!lst || !node)
+		return ;
+	if (!lst)
+		*lst = node;
+	else
+	{
+		(*lst)->prev->next = node;
+		
+
+int	main(int argc, char **argv)
+{
+	t_list	*stack_a;
+	t_list	*stack_b;
+
+	if (argc == 1)
+		return (0);
+	stack_a = NULL;
+	stack_b = NULL;
+	while (*(++argv))
+		init_stack(&stack_a, create_node(ft_atoi(*argv)));
