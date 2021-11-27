95c22462682dfae791379fdf84c7154cfd9d0ac7
zeroet <y36609672@gmail.com>
2021-11-28 00:38:59 +0100

    pipex

--- Pipex/pipex.c
+++ Pipex/pipex.c
@@ -6,7 +6,7 @@
 /*   By: seyun <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/27 14:01:54 by seyun             #+#    #+#             */
-/*   Updated: 2021/11/28 00:16:26 by seyun            ###   ########.fr       */
+/*   Updated: 2021/11/28 00:37:16 by seyun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,23 +20,24 @@ void	std_out(t_input *info)
 	if (fd < 0)
 		exit(1);
 	dup2(fd, 1);
-	close(fd);
+	//close(fd);
 }
 
 void	cmd2(int *fd, t_input *info)
 {
 	char	*path;
 	char	**argv;
-	int		i = -1;
 
+	close(fd[P_WRITE]);
 	dup2(fd[P_READ], 0);
 	std_out(info);
 	close(fd[P_READ]);
-	close(fd[P_WRITE]);
 	path = make_cmd(info, 3);
 	argv = ft_split(info->argv[3], ' ');
-	if (i == execve(path, argv, info->envp))
+	if (-1 == execve(path, argv, info->envp))
+	{	
 		ft_strexit("ERROR");
+	}
 }
 
 void	std_in(t_input *info)
@@ -47,23 +48,24 @@ void	std_in(t_input *info)
 	if (fd < 0)
 		exit(1);
 	dup2(fd, 0);
-	close(fd);
+	//close(fd);
 }
 
 void	cmd1(int *fd, t_input *info)
 {
 	char	*path;
 	char	**argv;
-	int		i =-1;
 
+	close(fd[P_READ]);
 	std_in(info);
 	dup2(fd[P_WRITE], 1);
 	close(fd[P_WRITE]);
-	close(fd[P_READ]);
 	path = make_cmd(info, 2);
 	argv = ft_split(info->argv[2], ' ');
-	if (i == execve(path, argv, info->envp))
+	if (-1 == execve(path, argv, info->envp))
+	{	
 		ft_strexit("ERROR");
+	}
 }
 
 void	pipex(t_input *info)
@@ -78,8 +80,9 @@ void	pipex(t_input *info)
 		cmd1(fd, info);
 	else
 	{
-		waitpid(pid, 0, 0);
+		waitpid(pid, 0, WNOHANG);
 		cmd2(fd, info);
+		exit(1);
 	}
 	exit(1);
 }
